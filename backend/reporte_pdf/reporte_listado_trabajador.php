9767a3db9b4b789e6ca93cae5d3f2026fa262fe9
gabriel valera <hashiramakage@gmail.com>
2021-05-23 20:16:17 -0400

    modificando formato ficha trabajadr

--- backend/reporte_pdf/reporte_listado_trabajador.php
+++ backend/reporte_pdf/reporte_listado_trabajador.php
@@ -8,7 +8,7 @@ class PdfTrabajador extends FPDF{
     private $datosCintillo;
     private $nombreCintillo;
     function __construct($datos,$generado,$datosCintillo){
-        parent::__construct("L","mm",array(325,325));
+        parent::__construct("L","mm","letter");
         $this->datosPdf=$datos;
         $this->generado=$generado;
         $this->datosCintillo=$datosCintillo;
@@ -42,7 +42,7 @@ class PdfTrabajador extends FPDF{
         $this->Cell(0,10,"FICHA DEL TRABAJADOR",0,0,"C");
 
         $this->ln(10);
-        $this->SetX(95);
+        $this->SetX(60);
         $this->SetFont("Arial","",10);
         $this->Cell(127,10,"",0,0,"L");
         $this->Cell(12,10,"Araure",0,0,"C");$this->Cell(1,10,"",0,0,"L");$this->Cell(10,6,date("d"),'B',0,"C");$this->Cell(1,10,"",0,0,"L");
@@ -52,104 +52,169 @@ class PdfTrabajador extends FPDF{
 
         $this->ln(15);
 
-        $this->SetFont("Arial","",10);
-        $this->Cell(12,10,"",0,0,"C");
-        $this->Cell(20,10,"Cedula:",0,0,"L");$this->Cell(14,10,utf8_decode($this->datosPdf[0]["id_cedula"]),0,0,"C");
+        // $this->SetFont("Arial","",10);
+        // $this->Cell(12,10,"",0,0,"C");
+        // $this->Cell(20,10,"Cedula:",0,0,"L");$this->Cell(14,10,utf8_decode($this->datosPdf[0]["id_cedula"]),0,0,"C");
 
-        $this->ln(8);
+        // $this->ln(8);
 
-        $this->SetFont("Arial","",10);
-        $this->Cell(12,10,"",0,0,"C");
-        $this->Cell(20,10,"Nombres:",0,0,"L");$this->Cell(18,10,utf8_decode($this->datosPdf[0]["nombres"]),0,0,"C");
+        // $this->SetFont("Arial","",10);
+        // $this->Cell(12,10,"",0,0,"C");
+        // $this->Cell(20,10,"Nombres:",0,0,"L");$this->Cell(18,10,utf8_decode($this->datosPdf[0]["nombres"]),0,0,"C");
 
-        $this->ln(8);
+        // $this->ln(8);
 
-        $this->SetFont("Arial","",10);
-        $this->Cell(12,10,"",0,0,"C");
-        $this->Cell(20,10,"Apellidos:",0,0,"L");$this->Cell(19,10,utf8_decode($this->datosPdf[0]["apellidos"]),0,0,"C");
+        // $this->SetFont("Arial","",10);
+        // $this->Cell(12,10,"",0,0,"C");
+        // $this->Cell(20,10,"Apellidos:",0,0,"L");$this->Cell(19,10,utf8_decode($this->datosPdf[0]["apellidos"]),0,0,"C");
 
-        $this->ln(8);
+        // $this->ln(8);
 
-        $this->SetFont("Arial","",10);
-        $this->Cell(12,10,"",0,0,"C");
-        $this->Cell(20,10,"Telefono movil:",0,0,"L");$this->Cell(36,10,utf8_decode($this->datosPdf[0]["telefono_movil"]),0,0,"C");
+        // $this->SetFont("Arial","",10);
+        // $this->Cell(12,10,"",0,0,"C");
+        // $this->Cell(20,10,"Telefono movil:",0,0,"L");$this->Cell(36,10,utf8_decode($this->datosPdf[0]["telefono_movil"]),0,0,"C");
 
-        $this->ln(8);
+        // $this->ln(8);
 
-        $this->SetFont("Arial","",10);
-        $this->Cell(12,10,"",0,0,"C");
-        $this->Cell(20,10,"Telefono local:",0,0,"L");$this->Cell(36,10,utf8_decode($this->datosPdf[0]["telefono_local"]),0,0,"C");
+        // $this->SetFont("Arial","",10);
+        // $this->Cell(12,10,"",0,0,"C");
+        // $this->Cell(20,10,"Telefono local:",0,0,"L");$this->Cell(36,10,utf8_decode($this->datosPdf[0]["telefono_local"]),0,0,"C");
 
-        $this->ln(8);
+        // $this->ln(8);
+        // $fecha= date("d-m-Y",strtotime($this->datosPdf[0]["fecha_nacimiento"]));
+        // $this->SetFont("Arial","",10);
+        // $this->Cell(12,10,"",0,0,"C");
+        // $this->Cell(20,10,"Fecha de nacimiento:",0,0,"L");$this->Cell(49,10,"24/11/1998",0,0,"C");
+
+        // $this->ln(8);
+
+        // $nacimiento = new DateTime($this->datosPdf[0]["fecha_nacimiento"]);
+        // $ahora = new DateTime(date("Y-m-d"));
+        // $diferencia = $ahora->diff($nacimiento);
+
+        // $this->SetFont("Arial","",10);
+        // $this->Cell(12,10,"",0,0,"C");
+        // $this->Cell(20,10,"Edad:",0,0,"L");$this->Cell(-12,10,$diferencia->format("%y"),0,0,"C");
+
+        // $this->ln(8);
+
+        // $this->SetFont("Arial","",10);
+        // $this->Cell(12,10,"",0,0,"C");
+        // $this->Cell(20,10,"Grado de instruccion:",0,0,"L");$this->Cell(77,10,utf8_decode($this->datosPdf[0]["grado_instruccion"]),0,0,"C");
+
+        // $this->ln(8);
+
+        // $this->SetFont("Arial","",10);
+        // $this->Cell(12,10,"",0,0,"C");
+        // $this->Cell(20,10,"Titulo:",0,0,"L");$this->Cell(54,10,utf8_decode($this->datosPdf[0]["titulo_grado_instruccion"]),0,0,"C");
+
+        // $this->ln(8);
+
+        // $this->SetFont("Arial","",10);
+        // $this->Cell(12,10,"",0,0,"C");
+        // $this->Cell(20,10,"Direccion:",0,0,"L");$this->Cell(59,10,utf8_decode($this->datosPdf[0]["direccion"]),0,0,"C");
+
+        // $this->ln(8);
+
+        // $this->SetFont("Arial","",10);
+        // $this->Cell(12,10,"",0,0,"C");
+        // $this->Cell(20,10,"Tipo trabajador:",0,0,"L");$this->Cell(28,10,utf8_decode($this->datosPdf[0]["descripcion_tipo_trabajador"]),0,0,"C");
+
+        // $this->ln(8);
+
+        // $this->SetFont("Arial","",10);
+        // $this->Cell(12,10,"",0,0,"C");
+        // $this->Cell(20,10,"Funcion trabajador:",0,0,"L");$this->Cell(52,10,utf8_decode($this->datosPdf[0]["funcion_descripcion"]),0,0,"C");
+
+        // $this->ln(8);
+
+        // $this->SetFont("Arial","",10);
+        // $this->Cell(12,10,"",0,0,"C");
+        // $this->Cell(20,10,"Sexo:",0,0,"L");$this->Cell(1,10,(($this->datosPdf[0]["sexo_trabajador"]==="1")?"Masculino":"Femenino"),0,0,"C");
+
+        // $this->ln(8);
+
+        // $this->SetFont("Arial","",10);
+        // $this->Cell(12,10,"",0,0,"C");
+        // $this->Cell(20,10,"Designacion:",0,0,"L");$this->Cell(18,10,(($this->datosPdf[0]["designacion"]==="1")?"Interno":"Externo"),0,0,"C");
+
+        // $this->ln(8);
+
+        // $this->SetFont("Arial","",10);
+        // $this->Cell(12,10,"",0,0,"C");
+        // $this->Cell(20,10,"Estatus:",0,0,"L");$this->Cell(3,10,(($this->datosPdf[0]["estatu_trabajador"]==="1")?"Activo":"Inactivo"),0,0,"C");
+        // if($this->datosPdf[0]["estatu_trabajador"]==="0"){
+        //     $this->ln(8);
+        //     $this->SetFont("Arial","",10);
+        //     $this->Cell(12,10,"",0,0,"C");
+        //     $this->Cell(20,10,"Fecha inactividad:",0,0,"L");$this->Cell(59,10,date("d-m-Y",strtotime($this->datosPdf[0]["fecha_inactividad"])),0,0,"C");
+        // }
+
+        $this->SetX(50); 
+        $this->Cell(20,10,'Cedula:',0,0,'C');$this->Cell(20,10,utf8_decode($this->datosPdf[0]["id_cedula"]),0,0,'C');
+
+        $this->SetX(110); 
+        $this->Cell(20,10,'Nombre:',0,0,'C');$this->Cell(21,10,utf8_decode($this->datosPdf[0]["nombres"]),0,0,'C');
+
+        $this->SetX(170); 
+        $this->Cell(20,10,'Apellido:',0,0,'C');$this->Cell(20,10,utf8_decode($this->datosPdf[0]["apellidos"]),0,0,'C');
+
+        $this->ln(15);
+
+        $this->SetX(45); 
+        $this->Cell(18,10,'Telefono movil:',0,0,'C');$this->Cell(38,10,utf8_decode($this->datosPdf[0]["telefono_movil"]),0,0,'C');
+
+        $this->SetX(105); 
+        $this->Cell(25,10,'Telefono local:',0,0,'C');$this->Cell(32,10,utf8_decode($this->datosPdf[0]["telefono_local"]),0,0,'C');
+
+        $this->SetX(165); 
+        $this->Cell(28,10,'Correo:',0,0,'C');$this->Cell(38,10,utf8_decode($this->datosPdf[0]["correo"]),0,0,'C');
+
+        $this->ln(15);
         $fecha= date("d-m-Y",strtotime($this->datosPdf[0]["fecha_nacimiento"]));
-        $this->SetFont("Arial","",10);
-        $this->Cell(12,10,"",0,0,"C");
-        $this->Cell(20,10,"Fecha de nacimiento:",0,0,"L");$this->Cell(49,10,"24/11/1998",0,0,"C");
-
-        $this->ln(8);
-
+        $this->SetX(45); 
+        $this->Cell(18,10,'Fecha de nacimiento:',0,0,'C');$this->Cell(38,10,$fecha,0,0,'C');
         $nacimiento = new DateTime($this->datosPdf[0]["fecha_nacimiento"]);
         $ahora = new DateTime(date("Y-m-d"));
         $diferencia = $ahora->diff($nacimiento);
 
-        $this->SetFont("Arial","",10);
-        $this->Cell(12,10,"",0,0,"C");
-        $this->Cell(20,10,"Edad:",0,0,"L");$this->Cell(-12,10,$diferencia->format("%y"),0,0,"C");
+        $this->SetX(104); 
+        $this->Cell(35,10,'Edad:',0,0,'C');$this->Cell(5,10,$diferencia->format("%y"),0,0,'C');
 
-        $this->ln(8);
+        $this->SetX(165); 
+        $this->Cell(50,10,'Grado de instruccion:',0,0,'C');$this->Cell(10,10,utf8_decode($this->datosPdf[0]["grado_instruccion"]),0,0,'C');
 
-        $this->SetFont("Arial","",10);
-        $this->Cell(12,10,"",0,0,"C");
-        $this->Cell(20,10,"Grado de instruccion:",0,0,"L");$this->Cell(77,10,utf8_decode($this->datosPdf[0]["grado_instruccion"]),0,0,"C");
+        $this->ln(15);
 
-        $this->ln(8);
+        $this->SetX(36); 
+        $this->Cell(10,10,'Titulo:',0,0,'C');$this->Cell(50,10,utf8_decode($this->datosPdf[0]["titulo_grado_instruccion"]),0,0,'C');
 
-        $this->SetFont("Arial","",10);
-        $this->Cell(12,10,"",0,0,"C");
-        $this->Cell(20,10,"Titulo:",0,0,"L");$this->Cell(54,10,utf8_decode($this->datosPdf[0]["titulo_grado_instruccion"]),0,0,"C");
+        $this->SetX(102); 
+        $this->Cell(12,10,'Direccion:',0,0,'C');$this->Cell(55,10,utf8_decode($this->datosPdf[0]["direccion"]),0,0,'C');
 
-        $this->ln(8);
+        $this->SetX(166); 
+        $this->Cell(40,10,'Tipo trabajador:',0,0,'C');$this->Cell(12,10,utf8_decode($this->datosPdf[0]["descripcion_tipo_trabajador"]),0,0,'C');
 
-        $this->SetFont("Arial","",10);
-        $this->Cell(12,10,"",0,0,"C");
-        $this->Cell(20,10,"Direccion:",0,0,"L");$this->Cell(59,10,utf8_decode($this->datosPdf[0]["direccion"]),0,0,"C");
+        $this->ln(15);
 
-        $this->ln(8);
+        $this->SetX(46); 
+        $this->Cell(10,10,'Funcion trabajador:',0,0,'C');$this->Cell(50,10,utf8_decode($this->datosPdf[0]["funcion_descripcion"]),0,0,'C');
 
-        $this->SetFont("Arial","",10);
-        $this->Cell(12,10,"",0,0,"C");
-        $this->Cell(20,10,"Tipo trabajador:",0,0,"L");$this->Cell(28,10,utf8_decode($this->datosPdf[0]["descripcion_tipo_trabajador"]),0,0,"C");
+        $this->SetX(114); 
+        $this->Cell(10,10,'Sexo:',0,0,'C');$this->Cell(35,10,(($this->datosPdf[0]["sexo_trabajador"]==="1")?"Masculino":"Femenino"),0,0,'C');
 
-        $this->ln(8);
+        $this->SetX(165); 
+        $this->Cell(40,10,'Designacion:',0,0,'C');$this->Cell(12,10,(($this->datosPdf[0]["designacion"]==="1")?"Interno":"Externo"),0,0,'C');
 
-        $this->SetFont("Arial","",10);
-        $this->Cell(12,10,"",0,0,"C");
-        $this->Cell(20,10,"Funcion trabajador:",0,0,"L");$this->Cell(52,10,utf8_decode($this->datosPdf[0]["funcion_descripcion"]),0,0,"C");
+        $this->ln(15);
 
-        $this->ln(8);
+        $this->SetX(76); 
+        $this->Cell(10,10,'Estatus:',0,0,'C');$this->Cell(50,10,(($this->datosPdf[0]["estatu_trabajador"]==="1")?"Activo":"Inactivo"),0,0,'C');
 
-        $this->SetFont("Arial","",10);
-        $this->Cell(12,10,"",0,0,"C");
-        $this->Cell(20,10,"Sexo:",0,0,"L");$this->Cell(1,10,(($this->datosPdf[0]["sexo_trabajador"]==="1")?"Masculino":"Femenino"),0,0,"C");
-
-        $this->ln(8);
-
-        $this->SetFont("Arial","",10);
-        $this->Cell(12,10,"",0,0,"C");
-        $this->Cell(20,10,"Designacion:",0,0,"L");$this->Cell(18,10,(($this->datosPdf[0]["designacion"]==="1")?"Interno":"Externo"),0,0,"C");
-
-        $this->ln(8);
-
-        $this->SetFont("Arial","",10);
-        $this->Cell(12,10,"",0,0,"C");
-        $this->Cell(20,10,"Estatus:",0,0,"L");$this->Cell(3,10,(($this->datosPdf[0]["estatu_trabajador"]==="1")?"Activo":"Inactivo"),0,0,"C");
         if($this->datosPdf[0]["estatu_trabajador"]==="0"){
-            $this->ln(8);
-            $this->SetFont("Arial","",10);
-            $this->Cell(12,10,"",0,0,"C");
-            $this->Cell(20,10,"Fecha inactividad:",0,0,"L");$this->Cell(59,10,date("d-m-Y",strtotime($this->datosPdf[0]["fecha_inactividad"])),0,0,"C");
+            $this->SetX(135); 
+            $this->Cell(40,10,'Fecha de inactividad:',0,0,'C');$this->Cell(12,10,date("d-m-Y",strtotime($this->datosPdf[0]["fecha_inactividad"])),0,0,'C');
         }
-
         $this->ln(25);
         
         // //TABLA
@@ -190,7 +255,6 @@ class PdfTrabajador extends FPDF{
 
 
         $this->ln(30);
-        $this->SetX(40);
         $this->Cell(17,10,"",0,0,"C");
         $this->Cell(45,10,"Generado por:",0,0,"R");$this->Cell(40,6,utf8_decode($this->generado),'B',0,"C");
 
