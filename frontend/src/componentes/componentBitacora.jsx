13e425bd50fb987ed02988e6af3a52f3ad308c70
gabriel valera <hashiramakage@gmail.com>
2021-08-11 15:25:34 -0400

    agregando ip dinamica al modulo de bitacora

--- frontend/src/componentes/componentBitacora.jsx
+++ frontend/src/componentes/componentBitacora.jsx
@@ -1,7 +1,8 @@
 import React from "react"
 
 import {withRouter} from "react-router-dom"
-
+// IP servidor
+import servidor from '../ipServer.js'
 //JS
 import axios from 'axios'
 import $ from 'jquery'
@@ -97,7 +98,7 @@ class ComponentBitacora extends React.Component {
           if(localStorage.getItem("usuario")){
             var respuesta_servior=""
             const token=localStorage.getItem("usuario")
-            await axios.get(`http://localhost:8080/login/verificar-sesion${token}`)
+            await axios.get(`http://${servidor.ipServidor}:${servidor.servidorNode.puerto}/login/verificar-sesion${token}`)
             .then(async respuesta=>{
                 respuesta_servior=respuesta.data
                 if(respuesta_servior.usuario){
@@ -110,7 +111,7 @@ class ComponentBitacora extends React.Component {
   
       async consultarPerfilTrabajador(modulo,subModulo,idPerfil){
         let estado=false
-        await axios.get(`http://localhost:8080/configuracion/acceso/consultar/${idPerfil}`)
+        await axios.get(`http://${servidor.ipServidor}:${servidor.servidorNode.puerto}/configuracion/acceso/consultar/${idPerfil}`)
         .then(repuesta => {
             let json=JSON.parse(JSON.stringify(repuesta.data))
             // console.log("datos modulos =>>>",json)
@@ -263,7 +264,7 @@ class ComponentBitacora extends React.Component {
             vitacora:datosFinales
         }
         $tablaBitacora.classList.add("ocultar")
-        axios.post("http://localhost:8080/transaccion/bitacora/consultar",json)
+        axios.post(`http://${servidor.ipServidor}:${servidor.servidorNode.puerto}/transaccion/bitacora/consultar`,json)
         .then(respuesta => {
             // alert("ok")
             let json=JSON.parse(JSON.stringify(respuesta.data))
