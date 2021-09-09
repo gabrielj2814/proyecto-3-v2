2a80f59c9bca6e6eaaaf7648cbe52073d9468970
19yisus <jesusitomorales70@gmail.com>
2021-09-09 12:10:58 -0400

    creando componentes de la vista representante

---
+++ frontend/src/componentes/ComponentRepresentante.jsx
@@ -0,0 +1,24 @@
+// id_cedula_representante char(8),
+// nombre_representante varchar(150),
+// apellidos_representante varchar(150),
+// fecha_nacimiento_representante date,
+// nivel_instruccion_representante varchar(150),
+// ocupacion_representante varchar(2000),
+// direccion_representante varchar(3000),
+// id_ciudad varchar(6),
+// telefono_movil_representante varchar(11)
+// telefono_local_representante varchar(11)
+// numero_hijos_representante varchar(2),
+// constitucion_familiar_representante varchar(150),
+// ingresos_representante varchar(10),
+// tipo_vivienda_representante char(1),
+//
+// numero_estudiante_inicial_representante varchar(2),
+// numero_estudiante_grado_1_representante varchar(2),
+// numero_estudiante_grado_2_representante varchar(2),
+// numero_estudiante_grado_3_representante varchar(2),
+// numero_estudiante_grado_4_representante varchar(2),
+// numero_estudiante_grado_5_representante varchar(2),
+// numero_estudiante_grado_6_representante varchar(2),
+//
+// estatus_representante char(1)
