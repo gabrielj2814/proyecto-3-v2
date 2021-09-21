a8ec279bd59af4a0ed086afcf9cfa5e38f5e32a0
gabriel valera <hashiramakage@gmail.com>
2021-09-21 19:43:54 -0400

    agregando funciones basica de registro, consulta y actualizacion del formulario del modulo de aula

--- frontend/src/componentes/componentAulaFormulario.jsx
+++ frontend/src/componentes/componentAulaFormulario.jsx
@@ -11,6 +11,7 @@ import servidor from '../ipServer.js'
 //componentes
 import ComponentDashboard from './componentDashboard'
 //sub componentes
+import AlertBootstrap from "../subComponentes/alertBootstrap"
 import InputButton from '../subComponentes/input_button'
 import ComponentFormCampo from '../subComponentes/componentFormCampo';
 import ComponentFormSelect from '../subComponentes/componentFormSelect';
@@ -26,13 +27,17 @@ class ComponentAulaFormulario extends React.Component{
     constructor(){
         super()
         this.mostrarModulo=this.mostrarModulo.bind(this)
+        this.cambiarEstado=this.cambiarEstado.bind(this)
+        this.operacion=this.operacion.bind(this)
         this.state={
             modulo:"",
             estado_menu:false,
             // formulario
+            id_aula:"",
             id_grado:"",
-            numero_grado:"",
-            estatus_grado:"1",
+            nombre_aula:"",
+            estatus_aula:"1",
+            listaGradosEscolares:[],
             // 
             msj_numero_grado:{
                 mensaje:"",
@@ -79,10 +84,230 @@ class ComponentAulaFormulario extends React.Component{
         }
     }
 
+    async componentWillMount(){
+        await this.consultarGradosEscolar()
+        const {operacion} = this.props.match.params
+        const {id} = this.props.match.params
+        if(operacion==="actualizar"){
+            await this.consultarAula(id)
+        }
+    }
+
+    cambiarEstado(a){
+        var input=a.target;
+        this.setState({[input.name]:input.value})
+    }
+
+    async consultarGradosEscolar(){
+        await axiosCustom.get(`configuracion/grado/consultar-todos`)
+        .then(respuesta => {
+            let respuestaServidor=JSON.parse(JSON.stringify(respuesta.data))
+            console.log(respuestaServidor)
+            if(respuestaServidor.estado_respuesta===true){
+                this.setState({listaGradosEscolares:respuestaServidor.datos})
+            }
+            else{
+                alert("este registro no exite")
+            }
+
+        })
+        .catch(error => {
+            console.error(`error de la peticion axios =>>> ${error}`)
+        })
+
+    }
+
+    extrarDatosDelFormData(formData){
+        let json={}
+        let iterador = formData.entries()
+        let next= iterador.next();
+        while(!next.done){
+            json[next.value[0]]=next.value[1]
+            next=iterador.next()
+        }
+        return json   
+    }
+
+    async consultarAula(id){
+        await axiosCustom.get(`configuracion/aula/consultar/${id}`)
+        .then(respuesta => {
+            console.log(respuesta.data)
+            let respuestaServidor=JSON.parse(JSON.stringify(respuesta.data))
+            this.setState(respuestaServidor.datos[0])
+            document.getElementById("id_grado").value=respuestaServidor.datos[0].id_grado
+        })
+        .catch(error => {
+            console.error(`error de la peticion axios =>>> ${error}`)
+        })
+    }
+
+
+    async operacion(){
+        const {operacion}=this.props.match.params
+        // alert("operacion")
+        const token=localStorage.getItem('usuario')
+        if(operacion==="registrar"){
+            // alert("Registrar")
+            let datosFormulario=new FormData(document.getElementById("formularioAula"))
+            let datosFormatiados=this.extrarDatosDelFormData(datosFormulario)
+            let datosAula={
+                aula:datosFormatiados,
+                token
+            }
+            // console.log(datosAula)
+            await axiosCustom.post("configuracion/aula/registrar",datosAula)
+            .then(respuesta => {
+                let respuestaServidor=JSON.parse(JSON.stringify(respuesta.data))
+                let alerta=JSON.parse(JSON.stringify(this.state.alerta))
+                // console.log(respuestaServidor)
+                alerta.color=respuestaServidor.color_alerta
+                alerta.mensaje=respuestaServidor.mensaje
+                if(respuestaServidor.estado_respuesta===false){
+                    alerta.estado=true
+                }
+                else{
+                    alerta.estado=respuestaServidor.estado_respuesta
+                }
+                this.setState({alerta})
+            })
+            .catch(error => {
+                console.error(`error de la peticion axios =>>> ${error}`)
+            })
+        }
+        else if(operacion==="actualizar"){
+            // alert("Registrar")
+            let datosFormulario=new FormData(document.getElementById("formularioAula"))
+            let datosFormatiados=this.extrarDatosDelFormData(datosFormulario)
+            let datosAula={
+                aula:datosFormatiados,
+                token
+            }
+            // console.log(datosAula)
+            await axiosCustom.put(`configuracion/aula/actualizar/${this.props.match.params.id}`,datosAula)
+            .then(respuesta => {
+                let respuestaServidor=JSON.parse(JSON.stringify(respuesta.data))
+                let alerta=JSON.parse(JSON.stringify(this.state.alerta))
+                // console.log(respuestaServidor)
+                alerta.color=respuestaServidor.color_alerta
+                alerta.mensaje=respuestaServidor.mensaje
+                if(respuestaServidor.estado_respuesta===false){
+                    alerta.estado=true
+                }
+                else{
+                    alerta.estado=respuestaServidor.estado_respuesta
+                }
+                this.setState({alerta})
+            })
+            .catch(error => {
+                console.error(`error de la peticion axios =>>> ${error}`)
+            })
+        }
+    }
+
     render(){
         const jsx=(
-            <div>
-                <h1>hola mundo</h1>
+            <div className="row justify-content-center">
+                {this.state.alerta.estado===true &&
+                    (<div className="col-12 col-ms-12 col-md-12 col-lg-12 col-xl-12">
+
+                        <AlertBootstrap colorAlert={this.state.alerta.color} mensaje={this.state.alerta.mensaje}/>
+                        
+                    </div>)
+                }
+                <div className="col-12 col-ms-12 col-md-12 col-lg-12 col-xl-12 contenedor_formulario_aula">
+                    <div className="row justify-content-center">
+                        <div className="col-12 col-ms-12 col-md-12 col-lg-12 col-xl-12 text-center contenedor-titulo-form-reposo">
+                            <span className="titulo-form-reposo">Formulario de Aula</span>
+                        </div>
+                    </div>
+                    <form id="formularioAula" >
+                        <div className="row justify-content-center">
+                            <ComponentFormCampo
+                            clasesColumna="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3"
+                            clasesCampo="form-control"
+                            nombreCampo="Código Aula:"
+                            activo="no"
+                            type="text"
+                            value={this.state.id_aula}
+                            name="id_aula"
+                            id="id_aula"
+                            placeholder="Código Aula"
+                            eventoPadre={this.cambiarEstado}
+                            />
+                            <ComponentFormCampo
+                            clasesColumna="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3"
+                            clasesCampo="form-control"
+                            nombreCampo="Nombre Aula:"
+                            activo="si"
+                            type="text"
+                            value={this.state.nombre_aula}
+                            name="nombre_aula"
+                            id="nombre_aula"
+                            placeholder="Nombre Aula"
+                            eventoPadre={this.cambiarEstado}
+                            />
+                            <div className="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+                                <div class="form-groud">
+                                    <label>Año de la Sección</label>
+                                    <select id="id_grado" name="id_grado" class="form-select custom-select" aria-label="Default select example" onChange={this.cambiarEstado}>
+                                        {this.state.listaGradosEscolares.map((grado,index)=> {
+                                            return(
+                                                <option key={index} value={grado.id_grado} >{grado.numero_grado}</option>
+                                            )
+                                            })
+
+                                        }
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row justify-content-center">
+                            <ComponentFormRadioState
+                            clasesColumna="col-9 col-ms-9 col-md-9 col-lg-9 col-xl-9"
+                            extra="custom-control-inline"
+                            nombreCampoRadio="Estatus:"
+                            name="estatus_aula"
+                            nombreLabelRadioA="Activo"
+                            idRadioA="activoA"
+                            checkedRadioA={this.state.estatus_aula}
+                            valueRadioA="1"
+                            nombreLabelRadioB="Inactivo"
+                            idRadioB="activoB"
+                            valueRadioB="0"
+                            eventoPadre={this.cambiarEstado}
+                            checkedRadioB={this.state.estatus_aula}
+                            />
+                        </div>
+                    </form>
+                    <div className="row justify-content-center">
+                            <div className="col-auto">
+                                {this.props.match.params.operacion==="registrar" &&
+                                    <InputButton 
+                                    clasesBoton="btn btn-primary"
+                                    id="boton-registrar"
+                                    value="Registrar"
+                                    eventoPadre={this.operacion}
+                                    />
+                                }
+                                {this.props.match.params.operacion==="actualizar" &&
+                                    <InputButton 
+                                    clasesBoton="btn btn-warning"
+                                    id="boton-actualizar"
+                                    value="Actualizar"
+                                    eventoPadre={this.operacion}
+                                    />   
+                                }
+                            </div>
+                            <div className="col-auto">
+                                <InputButton 
+                                clasesBoton="btn btn-danger"
+                                id="boton-cancelar"
+                                value="Cancelar"
+                                eventoPadre={this.regresar}
+                                />   
+                            </div>
+                        </div>
+                </div>
             </div>
         )
         return(
