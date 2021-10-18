12357c5815f22a78610dc57936c4e96ba9321757
ford2221 <fordstelle@gmail.com>
2021-10-18 11:27:02 -0400

    subiendo modulo año_escolar

---
+++ backend/rutas/configuracion/año-escolar/index.js
@@ -0,0 +1,16 @@
+const express = require("express"),
+  router = express.Router(),
+  bodyparser = require("body-parser"),
+  controladorAnoEscolar = require("../../../controlador/c_año_escolar")
+
+
+router.use(bodyparser.json())
+
+router.post("/registrar", controladorAnoEscolar.registrar_ano_escolar)//registrar
+router.get("/consultar-todos", controladorAnoEscolar.consultar_todos)// consultar todos
+router.get("/consultar-patron/:patron", controladorAnoEscolar.consultarpatron) //consultar por patron
+router.put("/actualizar/:id", controladorAnoEscolar.actualizar)// actualizar
+
+
+
+module.exports = router
