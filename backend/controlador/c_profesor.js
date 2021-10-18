f6f3120c5df8fa8f7a0288a88409b637639fc733
gabriel valera <hashiramakage@gmail.com>
2021-10-18 12:23:54 -0400

    agregando funcion de consultar todos del modulo profesor

--- backend/controlador/c_profesor.js
+++ backend/controlador/c_profesor.js
@@ -67,5 +67,27 @@ controladorProfesor.actualizar=async (req,res) => {
     res.end()
 }
 
+controladorProfesor.consultarTodos=async (req,res) => {
+    const respuesta_api={mensaje:"",datos:[],estado_respuesta:false,color_alerta:""}
+    const {id} = req.params
+    let modelo_profesor = new ModeloProfesor()
+    modelo_profesor.setIdProfesor(id)
+    let resultProfesor=await modelo_profesor.consultarTodos()
+    if(resultProfesor.rowCount>0){
+        respuesta_api.datos=resultProfesor.rows
+        respuesta_api.mensaje="consulta completada"
+        respuesta_api.estado_respuesta=true
+        respuesta_api.color_alerta="success"
+    }
+    else{
+        respuesta_api.mensaje="error al consultar"
+        respuesta_api.estado_respuesta=true
+        respuesta_api.color_alerta="danger"
+    }
+    res.writeHead(200,{"Content-Type":"application/json"})
+    res.write(JSON.stringify(respuesta_api))
+    res.end()
+}
+
 
 module.exports = controladorProfesor
--- backend/rutas/configuracion/profesor/index.js
+++ backend/rutas/configuracion/profesor/index.js
@@ -8,6 +8,7 @@ router.use(bodyparser.json())
 router.post("/registrar",controladorProfesor.registrar)
 router.get("/consultar/:id",controladorProfesor.consultar)
 router.put("/actualizar/:id",controladorProfesor.actualizar)
+router.get("/consultar-todos",controladorProfesor.consultarTodos)
 
 
 module.exports = router
