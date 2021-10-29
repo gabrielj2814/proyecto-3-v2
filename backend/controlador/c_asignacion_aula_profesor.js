f3c8c21094b4d564dfcba4ed03d24990952735f3
gabriel valera <hashiramakage@gmail.com>
2021-10-29 16:44:51 -0400

    agregando la seguientes funciones actualizar, consultar, consultar todos y consultar profesores por año escolar ( completamente funcionales )

--- backend/controlador/c_asignacion_aula_profesor.js
+++ backend/controlador/c_asignacion_aula_profesor.js
@@ -30,4 +30,90 @@ ControladorAsignacionAulaProfesor.registrar=async (req,res) => {
     res.end()
 }
 
+ControladorAsignacionAulaProfesor.consultar=async (req,res) => {
+    const respuesta_api={mensaje:"",datos:[],estado_respuesta:false,color_alerta:""}
+    let {id} = req.params
+    let modeloAsignacionAulaProfesor=new ModeloAsignacionAulaProfesor()
+    modeloAsignacionAulaProfesor.setdatoIdAsignacionAulaProfesor(id)
+    let resultAsignacionAulaProfesor= await modeloAsignacionAulaProfesor.consultar()
+    if(resultAsignacionAulaProfesor.rowCount>0){
+        respuesta_api.mensaje="consultar completada"
+        respuesta_api.datos=resultAsignacionAulaProfesor.rows
+        respuesta_api.estado_respuesta=true
+        respuesta_api.color_alerta="succes"
+    }
+    else{
+        respuesta_api.mensaje="error al consultar ( no se entrol el registro )"
+        respuesta_api.estado_respuesta=true
+        respuesta_api.color_alerta="danger"
+    }
+    res.writeHead(200,{"Content-Type":"application/json"})
+    res.write(JSON.stringify(respuesta_api))
+    res.end()
+}
+
+ControladorAsignacionAulaProfesor.consultarTodos=async (req,res) => {
+    const respuesta_api={mensaje:"",datos:[],estado_respuesta:false,color_alerta:""}
+    let modeloAsignacionAulaProfesor=new ModeloAsignacionAulaProfesor()
+    let resultAsignacionAulaProfesor= await modeloAsignacionAulaProfesor.consultarTodos()
+    if(resultAsignacionAulaProfesor.rowCount>0){
+        respuesta_api.mensaje="consultar completada"
+        respuesta_api.datos=resultAsignacionAulaProfesor.rows
+        respuesta_api.estado_respuesta=true
+        respuesta_api.color_alerta="succes"
+    }
+    else{
+        respuesta_api.mensaje="error al consultar ( no se entrol el registro )"
+        respuesta_api.estado_respuesta=true
+        respuesta_api.color_alerta="danger"
+    }
+    res.writeHead(200,{"Content-Type":"application/json"})
+    res.write(JSON.stringify(respuesta_api))
+    res.end()
+}
+
+ControladorAsignacionAulaProfesor.consultarPorAnoEscolar=async (req,res) => {
+    const respuesta_api={mensaje:"",datos:[],estado_respuesta:false,color_alerta:""}
+    let {id} = req.params
+    let modeloAsignacionAulaProfesor=new ModeloAsignacionAulaProfesor()
+    modeloAsignacionAulaProfesor.setdatoIdAnoEscolar(id)
+    let resultAsignacionAulaProfesor= await modeloAsignacionAulaProfesor.consultarTodosPorAnoEscolar()
+    if(resultAsignacionAulaProfesor.rowCount>0){
+        respuesta_api.mensaje="consultar completada"
+        respuesta_api.datos=resultAsignacionAulaProfesor.rows
+        respuesta_api.estado_respuesta=true
+        respuesta_api.color_alerta="succes"
+    }
+    else{
+        respuesta_api.mensaje="error al consultar ( no se entrol el registro )"
+        respuesta_api.estado_respuesta=true
+        respuesta_api.color_alerta="danger"
+    }
+    res.writeHead(200,{"Content-Type":"application/json"})
+    res.write(JSON.stringify(respuesta_api))
+    res.end()
+}
+
+ControladorAsignacionAulaProfesor.actualizar=async (req,res) => {
+    const respuesta_api={mensaje:"",estado_respuesta:false,color_alerta:""}
+    let {asignacionAulaProfesor} = req.body
+    let modeloAsignacionAulaProfesor=new ModeloAsignacionAulaProfesor()
+    modeloAsignacionAulaProfesor.setDatos(asignacionAulaProfesor)
+    let resultAsignacionAulaProfesor= await modeloAsignacionAulaProfesor.actualizar()
+    if(resultAsignacionAulaProfesor.rowCount>0){
+        respuesta_api.mensaje="actualización completada"
+        respuesta_api.estado_respuesta=true
+        respuesta_api.color_alerta="succes"
+    }
+    else{
+        respuesta_api.mensaje="error al actualizar ( no se entrol el registro )"
+        respuesta_api.estado_respuesta=true
+        respuesta_api.color_alerta="danger"
+    }
+    res.writeHead(200,{"Content-Type":"application/json"})
+    res.write(JSON.stringify(respuesta_api))
+    res.end()
+}
+
+
 module.exports= ControladorAsignacionAulaProfesor
--- backend/rutas/transaccion/asignacion-aula-profesor/index.js
+++ backend/rutas/transaccion/asignacion-aula-profesor/index.js
@@ -7,6 +7,10 @@ const ControladorAsignacionAulaProfesor=require("../../../controlador/c_asignaci
 router.use(bodyParser.json())
 
 router.post("/registrar",ControladorAsignacionAulaProfesor.registrar)
+router.get("/consultar/:id",ControladorAsignacionAulaProfesor.consultar)
+router.get("/consultar-todos",ControladorAsignacionAulaProfesor.consultarTodos)
+router.get("/consultar-por-ano-escolar/:id",ControladorAsignacionAulaProfesor.consultarPorAnoEscolar)
+router.put("/actualizar/:id",ControladorAsignacionAulaProfesor.actualizar)
 
 
 module.exports = router
