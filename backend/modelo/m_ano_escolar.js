111d889bf565ef53f436801f878f6fc697405c6e
ford2221 <fordstelle@gmail.com>
2021-12-17 10:58:27 -0400

    agregando seguimiento al año escolar

--- backend/modelo/m_ano_escolar.js
+++ backend/modelo/m_ano_escolar.js
@@ -11,6 +11,7 @@ class ModuloAnoEscolar extends DriverPostgre {
     this.fecha_inicio_ano_escolar = ""
     this.fecha_cierre_ano_escolar = ""
     this.estatus_ano_escolar = ""
+    this.seguimiento_ano_escolar = ""
   }
 
   setDatos(anoescolar) {
@@ -20,6 +21,7 @@ class ModuloAnoEscolar extends DriverPostgre {
     this.fecha_inicio_ano_escolar = anoescolar.fecha_inicio_ano_escolar
     this.fecha_cierre_ano_escolar = anoescolar.fecha_cierre_ano_escolar
     this.estatus_ano_escolar = anoescolar.estatus_ano_escolar
+    this.seguimiento_ano_escolar = anoescolar.seguimiento_ano_escolar
   }
 
   setIdAnoEscolar(id){
@@ -29,7 +31,7 @@ class ModuloAnoEscolar extends DriverPostgre {
   async registrar(){
     const SQL = `INSERT INTO tano_escolar(ano_desde, ano_hasta, fecha_inicio_ano_escolar, fecha_cierre_ano_escolar, estatus_ano_escolar)
                   VALUES('${this.ano_desde}', '${this.ano_hasta}', '${this.fecha_inicio_ano_escolar}', '${this.fecha_cierre_ano_escolar}', 
-                  '${this.estatus_ano_escolar}')
+                  '${this.estatus_ano_escolar}', '${this.seguimiento_ano_escolar}')
                 `
     return await this.query(SQL)
   }
@@ -59,7 +61,7 @@ class ModuloAnoEscolar extends DriverPostgre {
 
   async actualizar(){
     const SQL = `UPDATE tano_escolar SET ano_desde ='${this.ano_desde}', ano_hasta=  '${this.ano_hasta}', fecha_inicio_ano_escolar='${this.fecha_inicio_ano_escolar}',
-                  fecha_cierre_ano_escolar='${this.fecha_cierre_ano_escolar}', estatus_ano_escolar= '${this.estatus_ano_escolar}'
+                  fecha_cierre_ano_escolar='${this.fecha_cierre_ano_escolar}', estatus_ano_escolar= '${this.estatus_ano_escolar}', seguimiento_ano_escolar='${this.seguimiento_ano_escolar}'
                `
     return await this.query(SQL)
   }
