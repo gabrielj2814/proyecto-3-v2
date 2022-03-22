be9e3aee4b793cf63494ce0d6f6b143507f43055
gabriel valera <hashiramakage@gmail.com>
2022-03-21 22:28:55 -0400

    SOY UN CHAD POR TABAJAR DIRECTAMENTE EN MAESTRO QUE VERGA SOY NO JODA

---
+++ backend/controlador/c_boleta.js
@@ -0,0 +1,21 @@
+const ControladorBoleta={}
+const ModeloBoleta= require("../modelo/m_Boleta")
+
+ControladorBoleta.registrar= async (req,res) => {
+
+}
+
+
+ControladorBoleta.registrarMasivo= async (req,res) => {
+
+}
+
+ControladorBoleta.consultarTodasLasBoletasDelInscripto= async (req,res) => {
+
+}
+
+ControladorBoleta.consultarTodasLasBoleta= async (req,res) => {
+
+}
+
+module.exports = ControladorBoleta
---
+++ backend/modelo/m_boleta.js
@@ -0,0 +1,57 @@
+const DriverPostgre=require("./driver_postgresql")
+const Moment=require("moment")
+
+class ModeloBoleta extends DriverPostgre{
+
+    constructor(){
+        super()
+        this.id_boleta=""
+        this.id_inscripcion=""
+        this.id_lapso_academico=""
+        this.observacion_boleta=""
+        this.fecha_boleta=""
+    }
+
+    setIdBoleta(id){
+        this.id_boleta=id
+    }
+    
+    setDatos(datos){
+        this.id_boleta=datos.id_boleta
+        this.id_inscripcion=datos.id_inscripcion
+        this.observacion_boleta=datos.observacion_boleta
+        this.fecha_boleta=datos.fecha_boleta
+    }
+
+    async registrar(){
+        let fecha=Moment().format("YYYY-MM-DD")
+        const SQL=`INSERT INTO tboleta(
+            id_inscripcion,
+            id_lapso_academico,
+            observacion_boleta,
+            fecha_boleta
+        )
+        VALUES(
+            ${this.id_inscripcion},
+            ${this.id_lapso_academico},
+            '${this.observacion_boleta}',
+            '${fecha}'
+        ) RETURNING id_boleta;`
+        return await this.query(SQL)
+
+    }
+
+    async consultaInscripcion(){
+        const SQL=`SELECT * FROM tboleta WHERE id_inscripcion=${this.id_inscripcion};`
+        return await this.query(SQL)
+    }
+    
+    async consultar(){
+        const SQL=`SELECT * FROM tboleta WHERE id_boleta=${this.id_boleta};`
+        return await this.query(SQL)
+    }
+
+
+}
+
+module.exports = ModeloBoleta
