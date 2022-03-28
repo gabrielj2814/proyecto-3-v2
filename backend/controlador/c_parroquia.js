826db3edacfe4a4f8624a0ae2fede2b9b6483939
ford2221 <fordstelle@gmail.com>
2022-03-28 16:41:43 -0400

    subiendo el modulo parroquia

---
+++ backend/controlador/c_parroquia.js
@@ -0,0 +1,126 @@
+const controladorParroquia = {}
+
+controladorParroquia.registrar_parroquia = async (req, res) => {
+  const respuesta_api = { mensaje: "", estado_respuesta: false, color_alerta: "" }
+  const ModeloParraquia = require("../modelo/m_parroquia");
+  let { parroquia } = req.body
+  let modeloParroquia = new ModeloParraquia()
+  modeloParroquia.setDatos(parroquia);
+  let resultParroquia = await modeloParroquia.registrar()
+  
+  if (resultParroquia.rowCount > 0) {
+    respuesta_api.mensaje = "registro completado"
+    respuesta_api.estado_respuesta = true
+    respuesta_api.color_alerta = "success"
+  }
+  else {
+    respuesta_api.mensaje = "error al registrar a la parroquia"
+    respuesta_api.estado_respuesta = false
+    respuesta_api.color_alerta = "danger"
+  }
+  res.writeHead(200, { "Content-Type": "application/json" })
+  res.write(JSON.stringify(respuesta_api))
+  res.end()
+}
+
+controladorParroquia.consultarTodos = async (req, res) => {
+  const respuesta_api = { mensaje: "", datos: [], estado_respuesta: false, color_alerta: "" }
+  const ModeloParraquia = require("../modelo/m_parroquia")
+  let modeloParroquia = new ModeloParraquia()
+  let resultParroquia = await modeloParroquia.consultarTodos()
+  if (resultParroquia.rowCount > 0) {
+    respuesta_api.mensaje = "consulta completada"
+    respuesta_api.datos = resultParroquia.rows
+    respuesta_api.estado_respuesta = true
+    respuesta_api.color_alerta = "success"
+  }
+  else {
+    respuesta_api.mensaje = "no se a encontrado registro en la base de datos"
+    respuesta_api.estado_respuesta = false
+    respuesta_api.color_alerta = "danger"
+  }
+  res.writeHead(200, { "Content-Type": "application/json" })
+  res.write(JSON.stringify(respuesta_api))
+  res.end()
+}
+
+controladorParroquia.consultar = async (req, res) => {
+  const respuesta_api = { mensaje: "", datos: [], estado_respuesta: false, color_alerta: "" }
+  const ModeloParraquia = require("../modelo/m_parroquia")
+  let { id } = req.params
+  let modeloParroquia = new ModeloParraquia()
+  modeloParroquia.setIdParraquia(id)
+  let resultParroquia = await modeloParroquia.consultar()
+  if (resultParroquia.rowCount > 0) {
+    respuesta_api.mensaje = "consulta completada"
+    respuesta_api.datos = resultParroquia.rows
+    respuesta_api.estado_respuesta = true
+    respuesta_api.color_alerta = "success"
+  }
+  else {
+    respuesta_api.mensaje = "no se a encontrado el registro en la base de datos"
+    respuesta_api.estado_respuesta = false
+    respuesta_api.color_alerta = "danger"
+  }
+  res.writeHead(200, { "Content-Type": "application/json" })
+  res.write(JSON.stringify(respuesta_api))
+  res.end()
+}
+
+controladorParroquia.consultarpatron = async (req, res) => {
+  const respuesta_api = { mensaje: "", datos: [], estado_respuesta: false, color_alerta: "" }
+  const ModeloParraquia = require("../modelo/m_parroquia");
+  const patron = req.params.patron
+  let modeloParroquia = new ModeloParraquia()
+  let resultParroquia = await modeloParroquia.consultarpatron(patron)
+
+  if (resultParroquia.rowCount > 0) {
+    respuesta_api.mensaje = "consulta completada"
+    respuesta_api.datos = resultParroquia.rows
+    respuesta_api.estado_respuesta = true
+    respuesta_api.color_alerta = "success"
+  }
+  else {
+    respuesta_api.mensaje = "no se a encontrado registro en la base de datos"
+    respuesta_api.estado_respuesta = false
+    respuesta_api.color_alerta = "danger"
+  }
+  res.writeHead(200, { "Content-Type": "application/json" })
+  res.write(JSON.stringify(respuesta_api))
+  res.end()
+}
+
+controladorParroquia.actualizar = async (req, res) => {
+  const respuesta_api = { mensaje: "", estado_respuesta: false, color_alerta: "" }
+  const ModeloParraquia = require("../modelo/m_parroquia")
+  let { parroquia } = req.body
+  let { id } = req.params
+  let modeloParroquia = new ModeloParraquia()
+  modeloParroquia.setDatos(parroquia)
+  modeloParroquia.setIdParraquia(id)
+  let resultParroquia = await modeloParroquia.consultar()
+  if (resultParroquia.rowCount > 0) {
+    let resultParroquia2 = await modeloParroquia.actualizar()
+    if (resultParroquia2.rowCount > 0) {
+      respuesta_api.mensaje = "actualización completada"
+      respuesta_api.estado_respuesta = true
+      respuesta_api.color_alerta = "success"
+    }
+    else {
+      respuesta_api.mensaje = "error al actualizar"
+      respuesta_api.estado_respuesta = false
+      respuesta_api.color_alerta = "danger"
+    }
+  }
+  else {
+    respuesta_api.mensaje = "error al actualizar (este registro no se encuentra en la base de datos)"
+    respuesta_api.estado_respuesta = false
+    respuesta_api.color_alerta = "warning"
+  }
+  res.writeHead(200, { "Content-Type": "application/json" })
+  res.write(JSON.stringify(respuesta_api))
+  res.end()
+}
+
+
+module.exports = controladorParroquia;
---
+++ backend/modelo/m_parroquia.js
@@ -0,0 +1,58 @@
+const DriverPostgres = require("./driver_postgresql")
+
+class ModuloParroquia extends DriverPostgres {
+  constructor() {
+    super();
+    this.id_parroquia = ""
+    this.nombre_parroquia = ""
+    this.id_ciudad = ""
+    this.estatu_parroquia = ""
+  }
+
+  setDatos(parroquia){
+    this.id_parroquia = parroquia.id_parroquia
+    this.nombre_parroquia = parroquia.nombre_parroquia
+    this.id_ciudad = parroquia.id_ciudad
+    this.estatu_parroquia = parroquia.estatu_parroquia
+  }
+
+  setIdParraquia(id){
+    this.id_parroquia = id
+  }
+
+  async registrar() {
+    const SQL = ` INSERT INTO tparroquia (nombre_parroquia,id_ciudad,estatu_parroquia) VALUES ('${this.nombre_parroquia}',
+                  '${this.id_ciudad}','${this.estatu_parroquia}')
+    `
+    return await this.query(SQL)
+  }
+
+  async consultarTodos() {
+    const SQL = 'SELECT * FROM tparroquia'
+
+    return await this.query(SQL);
+  }
+
+  async consultar() {
+    const SQL = `SELECT * FROM tparroquia JOIN tciudad ON tciudad.id_ciudad = tparroquia.id_ciudad WHERE 
+    tparroquia.id_parroquia=${this.id_parroquia}`
+
+    return await this.query(SQL);
+  }
+
+  async consultarpatron(patron) {
+    const SQL = `SELECT * FROM tparroquia WHERE nombre_parroquia LIKE '%${patron}%'`
+
+    return await this.query(SQL);
+  }
+
+  async actualizar(){
+    const SQL = ` UPDATE tparroquia SET nombre_parroquia= '${this.nombre_parroquia}', id_ciudad= '${this.id_ciudad}', 
+                  estatu_parroquia= '${this.estatu_parroquia}' 
+    
+    `
+    return await this.query(SQL);
+  }
+}
+
+module.exports = ModuloParroquia;
