0d2b7f813201fab4b80bd2e211e6fdd14f26f045
gabriel valera <hashiramakage@gmail.com>
2022-04-20 20:42:59 -0400

    agregando modal con filtros de grado y aula funcionales

--- frontend/src/componentes/componentAsignacionAulaProfesor.jsx
+++ frontend/src/componentes/componentAsignacionAulaProfesor.jsx
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap-grid.css'
 import '../css/componentAsignacionAulaProfesor.css'
 //JS
 import axios from 'axios'
+import $ from 'jquery'
 // IP servidor
 import servidor from '../ipServer.js'
 //componentes
@@ -31,10 +32,17 @@ class ComponentAsignacionAulaProfesor extends React.Component{
         this.consultarAsignacionesPorAnoEscolar=this.consultarAsignacionesPorAnoEscolar.bind(this)
         this.irAlFormularioDeActualizacion=this.irAlFormularioDeActualizacion.bind(this)
         this.irAlFormularioDeRegistro=this.irAlFormularioDeRegistro.bind(this)
+        this.mostrarModalPdf=this.mostrarModalPdf.bind(this)
+        this.consultarTodasLasAulaPorGrado=this.consultarTodasLasAulaPorGrado.bind(this)
+        this.mostrarFiltros=this.mostrarFiltros.bind(this)
         // this.cambiarEstado=this.cambiarEstado.bind(this)
         this.state={
             modulo:"",
             estado_menu:false,
+            id_grado:null,
+            id_aula:null,
+            listaGrados:[],
+            listaAulasPorGrado:[],
             //------------------ 
             listaAnoEscolares:[],
             registros:[],
@@ -42,7 +50,11 @@ class ComponentAsignacionAulaProfesor extends React.Component{
                 color:null,
                 mensaje:null,
                 estado:false
-            }
+            },
+            // 
+            tipoPdf:null,
+            id_cedula:null,
+            nombre_usuario:null,
         }
     }
 
@@ -79,6 +91,7 @@ class ComponentAsignacionAulaProfesor extends React.Component{
     }
 
     async componentWillMount(){
+        await this.consultarTodosLosGrados()
         let acessoModulo=await this.validarAccesoDelModulo("/dashboard/transaccion","/asignacion-aula-profesor")
         if(acessoModulo){
             await this.consultarAnoEscolares()
@@ -90,17 +103,46 @@ class ComponentAsignacionAulaProfesor extends React.Component{
         }
     }
 
+    async consultarTodosLosGrados(){
+        await axiosCustom.get(`configuracion/grado/consultar-todos`)
+        .then(respuesta => {
+            let repuestaServidor=JSON.parse(JSON.stringify(respuesta.data))
+            console.log(repuestaServidor)
+            this.setState({listaGrados:repuestaServidor.datos})
+        })
+        .catch(error => {
+            console.error("error =>>> ",error)
+        })
+    }
+
+    async consultarTodasLasAulaPorGrado(a){
+        let $select=a.target
+        this.mostrarFiltros($select)
+        this.setState({id_grado:$select.value})
+        await axiosCustom.get(`configuracion/aula/consultar-aula-por-grado/${$select.value}`)
+        .then(respuesta => {
+            let repuestaServidor=JSON.parse(JSON.stringify(respuesta.data))
+            console.log(repuestaServidor)
+            this.setState({listaAulasPorGrado:repuestaServidor.datos})
+        })
+        .catch(error => {
+            console.error("error =>>> ",error)
+        })
+    }
+
     async validarAccesoDelModulo(modulo,subModulo){
         // /dashboard/configuracion/acceso
         let estado = false
-          if(localStorage.getItem("usuario")){
+        if(localStorage.getItem("usuario")){
             var respuesta_servior=""
             const token=localStorage.getItem("usuario")
             await axios.get(`http://${servidor.ipServidor}:${servidor.servidorNode.puerto}/login/verificar-sesion${token}`)
             .then(async respuesta=>{
                 respuesta_servior=respuesta.data
                 if(respuesta_servior.usuario){
-                  estado=await this.consultarPerfilTrabajador(modulo,subModulo,respuesta_servior.usuario.id_perfil)
+                    this.setState({id_cedula:respuesta_servior.usuario.id_cedula})
+                    this.setState({nombre_usuario:respuesta_servior.usuario.nombre_usuario})
+                    estado=await this.consultarPerfilTrabajador(modulo,subModulo,respuesta_servior.usuario.id_perfil)
                 }  
             })
         }
@@ -195,6 +237,66 @@ class ComponentAsignacionAulaProfesor extends React.Component{
         this.props.history.push(`/dashboard/transaccion/asignacion-aula-profesor/registrar`)
     }
 
+    mostrarModalPdf(){
+        $("#modalPdf").modal("show")
+    }
+
+    mostrarFiltros(a){
+        let $select=a.target
+        let $filaVerPdf=document.getElementById("filaVerPdf")
+        $filaVerPdf.classList.add("ocultarFormulario")
+        // alert($select.value)
+        let $botonGenerarPdf=document.getElementById("botonGenerarPdf")
+        let $selectGrado=document.getElementById("selectGrado")
+        let $selectAula=document.getElementById("selectAula")
+        this.setState({id_aula:$selectAula.value})
+        if($selectAula.value!=="null" && $selectGrado.value!=="null"){
+            $botonGenerarPdf.classList.remove("ocultarFormulario")
+            this.setState({tipoPdf:true})
+        }
+        else{
+            this.setState({tipoPdf:false})
+            $botonGenerarPdf.classList.add("ocultarFormulario")
+        }
+    }
+
+    generarPdf(){
+        let $filaVerPdf=document.getElementById("filaVerPdf")
+        // $filaVerPdf.classList.remove("ocultarFormulario") //esta line sirve para mostrar el boton para ver el pdf => usar en success
+        // $filaVerPdf.classList.add("ocultarFormulario") //esta line sirve para ocultar el boton para ver el pdf => usar en error
+        let datos=[]
+        // console.log(datos)
+        datos.push({name:"nombre_usuario",value:this.state.nombre_usuario})
+        datos.push({name:"tipoPdf",value:this.state.tipoPdf})
+        datos.push({name:"cedula_usuario",value:this.state.id_cedula})
+        console.log(datos)
+        // if(datos!==null){
+        //   // alert("generar pdf")
+        //   $.ajax({
+        //     url: `http://${servidor.ipServidor}:${servidor.servidorApache.puerto}/proyecto/backend/controlador_php/controlador_lista_profesor.php`,
+        //     type:"post",
+        //     data:datos,
+        //     success: function(respuesta) {
+        //         console.log(respuesta)
+        //         let json=JSON.parse(respuesta)
+        //         console.log("datos reporte martricula =>>>> ",json)
+        //         if(json.nombrePdf!=="false"){
+        //             $filaVerPdf.classList.remove("ocultarFormulario") 
+        //             document.getElementById("linkPdf").href=`http://${servidor.ipServidor}:${servidor.servidorNode.puerto}/reporte/${json.nombrePdf}`
+        //         }
+        //         else{
+        //             $filaVerPdf.classList.add("ocultarFormulario") 
+        //             alert("no se pudo generar el pdf por que no hay registros que coincidan con los datos enviados")
+        //         }
+        //     },
+        //     error: function() {
+        //     //   alert("error")
+        //       $filaVerPdf.classList.add("ocultarFormulario") 
+        //     }
+        //   });
+        // }
+    }
+
     render(){
 
         const jsx_tabla_encabezado=(
@@ -240,6 +342,62 @@ class ComponentAsignacionAulaProfesor extends React.Component{
 
         const jsx=(
             <div>
+                <div class="modal fade" id="modalPdf" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-lg" role="document">
+                            <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Reporte pdf</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                            <div className="row justify-content-center mb-3">
+                                <div className="col-5 col-sm-5 col-md-5 col-lg-5 col-xl-5">
+                                  <div class="form-groud">
+                                    <label>Grado</label>
+                                    <select id="selectGrado" class="form-select custom-select" aria-label="Default select example"onChange={this.consultarTodasLasAulaPorGrado} >
+                                      <option value="null" >Seleccione Un Grado</option>
+                                      {this.state.listaGrados.map((grado, index) => {
+                                          return (
+                                            <option value={grado.id_grado} key={index}>{grado.numero_grado}</option>
+                                          )
+                                        })
+
+                                      }
+                                    </select>
+                                  </div>
+                                </div>
+                                <div className="col-5 col-sm-5 col-md-5 col-lg-5 col-xl-5">
+                                  <div class="form-groud">
+                                    <label>Aula</label>
+                                    <select id="selectAula" class="form-select custom-select" aria-label="Default select example" onChange={this.mostrarFiltros}>
+                                      <option value="null" >Seleccione Una Aula</option>
+                                      {this.state.listaAulasPorGrado.map((aula, index) => {
+                                          return (
+                                            <option value={aula.id_aula} key={index}>{aula.nombre_aula}</option>
+                                          )
+                                        })
+
+                                      }
+                                    </select>
+                                  </div>
+                                </div>
+                            </div>
+                             
+                              <div id="filaVerPdf" className="row justify-content-center ocultarFormulario">
+                                  <div className="col-auto">
+                                    <a className="btn btn-success" id="linkPdf" target="_blank" href="#">Ver pdf</a>
+                                  </div>
+                              </div>
+                              
+                            </div>
+                            <div class="modal-footer ">
+                                <button type="button" id="botonGenerarPdf" class="btn btn-success ocultarFormulario" onClick={this.generarPdf}>Generar pdf</button>
+                            </div>
+                            </div>
+                        </div>
+                  </div>
                 {this.state.alerta.estado===true &&
                     (<div className="col-12 col-ms-12 col-md-12 col-lg-12 col-xl-12">
 
@@ -268,14 +426,21 @@ class ComponentAsignacionAulaProfesor extends React.Component{
                     </div>
                 </div>
 
-                <div className="row">
+                <div className="row justify-content-between">
                 
-                  <div className="col-3 col-ms-3 col-md-3 columna-boton">
+                    <div className="col-3 col-ms-3 col-md-3 columna-boton">
                       <div className="row justify-content-center align-items-center contenedor-boton">
                         <div className="col-auto">
                           <InputButton clasesBoton="btn btn-primary" eventoPadre={this.irAlFormularioDeRegistro} value="Registrar"/>
                         </div>
                       </div>
+                    </div>
+                    <div className="col-3 col-ms-3 col-md-3 columna-boton">
+                        <div className="row justify-content-center align-items-center contenedor-boton">
+                            <div className="col-auto">
+                                <InputButton clasesBoton="btn btn-danger" eventoPadre={this.mostrarModalPdf} value="pdf"/>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
